1fe493d07d609ba383cd614de10b60a638336a7f
unknown <radu.dumitrache@student.nhlstenden.com>
2024-06-20 02:11:28 +0300

    added the landing page

--- app/src/main/java/com/example/appdev/LandUpActivity.kt
+++ app/src/main/java/com/example/appdev/LandUpActivity.kt
@@ -1,11 +1,24 @@
 package com.example.appdev
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class LandUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_landing_up)
+        val registerButton = findViewById<Button>(R.id.register_button)
+        val loginButton = findViewById<Button>(R.id.login_button)
+        registerButton.setOnClickListener {
+
+        }
+        loginButton.setOnClickListener {
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
         }
 }
