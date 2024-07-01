411f7b7a640d71b9ce2a944000101de57a12a43d
gtomkiel <grzegorz.tomkiel@student.nhlstenden.com>
2024-07-01 16:26:01 +0200

    Redesign of several pages

--- app/src/main/java/com/example/appdev/ui/dashboard/TransactionAdapter.kt
+++ app/src/main/java/com/example/appdev/ui/dashboard/TransactionAdapter.kt
@@ -4,11 +4,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.appdev.R
 
 class TransactionAdapter(private val transactions: List<DashboardFragment.Transaction>) :
     RecyclerView.Adapter<TransactionAdapter.TransactionViewHolder>(){
+
     class TransactionViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val dateTextView: TextView = itemView.findViewById(R.id.transaction_date)
         val descriptionTextView: TextView = itemView.findViewById(R.id.transaction_description)
@@ -26,8 +28,15 @@ class TransactionAdapter(private val transactions: List<DashboardFragment.Transa
         holder.dateTextView.text = currentTransaction.date
         holder.descriptionTextView.text = currentTransaction.description
         holder.amountTextView.text = currentTransaction.amount
+
+        // Set the text color based on the amount
+        val amountValue = currentTransaction.amount.replace("[$,]".toRegex(), "").toFloat()
+        if (amountValue < 0) {
+            holder.amountTextView.setTextColor(ContextCompat.getColor(holder.itemView.context, R.color.negativeAmount))
+        } else {
+            holder.amountTextView.setTextColor(ContextCompat.getColor(holder.itemView.context, R.color.positiveAmount))
+        }
     }
 
     override fun getItemCount() = transactions.size
-
 }
