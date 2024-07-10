78cfe83796739c83a843c42ef0df0296a2d3c810
unknown <radu.dumitrache@student.nhlstenden.com>
2024-07-11 01:58:55 +0300

    linked the account page to the database and added the logout button

--- app/src/main/java/com/example/appdev/database/daos/UserDao.kt
+++ app/src/main/java/com/example/appdev/database/daos/UserDao.kt
@@ -4,6 +4,8 @@ import androidx.room.Insert
 import androidx.room.Query
 import com.example.appdev.database.entities.UserEntity
 import androidx.room.OnConflictStrategy
+import androidx.room.Update
+
 @Dao
 interface UserDao {
 
@@ -15,4 +17,8 @@ interface UserDao {
 
     @Query("SELECT * FROM User WHERE email = :email")
     fun getUserByEmail(email :String): UserEntity?
+    @Query("SELECT * From User WHERE user_id = :id")
+    fun getUserById(id : Int) : UserEntity
+    @Update
+    fun updateUser(user: UserEntity)
 }
--- app/src/main/java/com/example/appdev/ui/account/AccountViewModel.kt
+++ app/src/main/java/com/example/appdev/ui/account/AccountViewModel.kt
@@ -1,7 +1,24 @@
 package com.example.appdev.ui.account
 
 import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.appdev.database.GoalSaverDatabase
+import com.example.appdev.database.entities.UserEntity
+class AccountViewModel(application: Application) : AndroidViewModel(application) {
+    private val userDao = GoalSaverDatabase.getDatabase(application).userDao()
+    private val _user = MutableLiveData<UserEntity>()
+    val user: LiveData<UserEntity> get() = _user
 
-class AccountViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    fun loadUser(userId: Int) {
+        val user = userDao.getUserById(userId)
+        _user.value = user
+    }
+
+    fun updateUser(user: UserEntity) {
+        userDao.updateUser(user)
+        _user.value = user
+    }
 }
